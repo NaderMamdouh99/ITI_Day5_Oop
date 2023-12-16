cb8f6b01492300799231da79fc2d0a93092daa71
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 16:20:12 +0200

    Add project files.

---
+++ Classes/Car.cs
@@ -0,0 +1,53 @@
+﻿using Day7.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7.Classes
+{
+    internal class Car:IVehicle , IDrivable
+    {
+        private int Gasoline { get; set; }   
+        public Car( int _start_gasoline )
+        {
+            Gasoline = _start_gasoline;
+        }
+		#region Implicitly
+
+		public void Drive()
+        {
+            if (Gasoline > 0)
+            {
+                Console.WriteLine("The car is Driving");
+            }
+            else
+            {
+                Console.WriteLine("The Car Not Can Driving");
+            }
+        }
+
+        public bool Refuel(int gasoline)
+        {
+            Gasoline = Gasoline + gasoline;
+            return gasoline > 0;
+        } 
+        #endregion
+
+        #region Explicitly
+        void IDrivable.Move()
+        {
+            Console.WriteLine("Car Is Move ");
+        }
+		void IDrivable.Accelerate()
+        {
+            Console.WriteLine("Accelerate Is Down");
+        }
+        void IDrivable.Drive()
+        {
+            Console.WriteLine("The Car is Driving ");
+        } 
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Day7.Interface;
+using System.Reflection.Metadata;
+
+namespace Day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+		{
+			#region Assuignment
+				//1-Create IVehicle interface with two methods, one for Drive of type void and another for Refuel of type bool that has a parameter of type integer with the amount of gasoline to refuel.
+				//2-Then create a Car class with a constructor that receives a parameter with the car's starting gasoline amount and implements the Drive and Refuel methods of the car.
+				//3-The Drive method will print on the screen that the car is Driving, if the gasoline is greater than 0. 
+				//4-The Refuel method will increase the gasoline of the car and return true.
+				//5-Create IDrivable interface that has 3 methods, one for Move of type void and one for Accelerate of type void and one for Drive of type void.
+				//6-Make class Car implement IDrivable.
+				//a.Can class Car implement more than one Interface?
+				//b.Try implementing the interfaces explicitly and implicitly.
+
+			#endregion
+		}
+}
+}
